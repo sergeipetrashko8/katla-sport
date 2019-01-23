7ee1d43d820375dc3293d65614d6d34838c5c3c7
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-01-23 16:50:50 +0300

    Merge step14. Added the ability to create a new storage section, edit and delete an existing section.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HiveSection } from '../models/hive-section';
+import { HiveSectionService } from '../services/hive-section.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveService } from '../services/hive.service';
+import { HiveListItem } from '../models/hive-list-item';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", 0, false, "");
+  existed: boolean = false;
+  hiveId: number;
+  hives: HiveListItem[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(p => {
+      if (p['id'] === undefined) return;
+      this.hiveSectionService.getHiveSection(p['id']).subscribe(h => this.hiveSection = h);
+      this.existed = true;
+    });
+
+    this.hiveService.getHives().subscribe(h => this.hives = h);
+  }
+
+  navigateToHiveSections() {
+    if (this.hiveId === undefined){
+      this.hiveId = this.hiveSection.storeHiveId;
+    }
+
+    this.router.navigate([`/hive/${this.hiveId}/sections`]);
+  }
+
+  onCancel() {
+    this.navigateToHiveSections();
+  }
+  
+  onSubmit() {
+    if (this.existed) {
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(c => this.navigateToHiveSections());
+    } else {
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(c => this.navigateToHiveSections());
+    }
+  }
+
+  onDelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(c => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, false).subscribe(c => this.hiveSection.isDeleted = false);
+  }
+
+  onPurge() {
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(c => this.navigateToHiveSections());
   }
 }
--- KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
@@ -24,4 +24,16 @@ export class HiveSectionService {
   setHiveSectionStatus(hiveSectionId: number, deletedStatus: boolean): Observable<Object> {
     return this.http.put<HiveSection>(`${this.url}${hiveSectionId}/status/${deletedStatus}`, null);
   }
+
+  addHiveSection(hiveSection: HiveSection): Observable<HiveSection> {
+    return this.http.post<HiveSection>(`${this.url}`, hiveSection);
+  }
+
+  updateHiveSection(hiveSection: HiveSection): Observable<HiveSection> {
+    return this.http.put<HiveSection>(`${this.url}${hiveSection.id}`, hiveSection);
+  }
+
+  deleteHiveSection(hiveSectionId: number): Observable<HiveSection> {
+    return this.http.delete<HiveSection>(`${this.url}${hiveSectionId}`);
+  }
 }
